60fd25dcb6fef6ccc3c49d5c4223da726934c31b
ali.khosro72@gmail.com <ali.khosro72@gmail.com>
2023-01-04 18:28:01 +0330

    feat: get query keys from react query and set that in fetcher function

--- src/services/characters/useCharacters.tsx
+++ src/services/characters/useCharacters.tsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import { useQuery } from "react-query";
 
+interface FetchParams {
+  queryKey: (string | number)[];
+}
+
 const useCharacters = () => {
-  const fetchCharacters = async () => {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+  const [page, setPage] = useState(1);
+
+  const fetchCharacters = async ({ queryKey }: FetchParams) => {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
+    );
     return response.json();
   };
 
-  const { data, status } = useQuery("characters", fetchCharacters);
+  const { data, status } = useQuery(["characters", page], fetchCharacters);
 
   return {
     data,
     status,
+    setPage,
   };
 };
 
